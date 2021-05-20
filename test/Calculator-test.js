ba5fe943317e4e27ad4d38057a62bea3621e9123
solenemhep <solene.pettier@gmail.com>
2021-05-20 17:02:06 +0200

    test non-fonctionels

---
+++ test/Calculator-test.js
@@ -0,0 +1,56 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Calculator", function () {
+  let Calculator, calculator
+
+  beforeEach(async () => {
+    Calculator = await ethers.getContractFactory('Calculator');
+    calculator = await Calculator.deploy();
+  })
+
+  describe('Deployment', () => {
+
+    it('Should set the right owner at deployment', async () => {
+      await calculator.deployed()
+      const owner = await ethers.getSigners();
+      expect(owner).to.equal(owner.address);
+    });
+
+  })
+
+  describe('Calculation', () => {
+
+    it('Should calculate the right result : addition', async function () {
+      const result = await calculator.add(10, 5);
+      expect(result).to.equal(15);
+    });
+
+    it('Should calculate the right result : substraction', async function () {
+      const result = await calculator.sub(10, 5);
+      expect(result).to.equal(5);
+    });
+
+    it('Should calculate the right result : multiplication', async function () {
+      const result = await calculator.mul(10, 5);
+      expect(result).to.equal(50);
+    });
+
+    it('Should calculate the right result : division', async function () {
+      const result = await calculator.div(10, 5);
+      expect(result).to.equal(2);
+    });
+
+    it('Should revert if nb2 equals 0', async function () {
+      const result = await calculator.div(10, 0);
+      expect(result).to.be.reverted;
+    });
+
+    it('Should calculate the right result : modulo', async function () {
+      const result = await calculator.mod(10, 5);
+      expect(result).to.equal(0);
+    });
+
+  })
+
+});
