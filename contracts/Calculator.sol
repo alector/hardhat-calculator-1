73f46f2becd6d9b2c1c848086ca24834c5c5c61b
solenemhep <solene.pettier@gmail.com>
2021-05-21 11:17:21 +0200

    live correction

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -4,24 +4,24 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract Calculator {
-    function add(uint256 nb1, uint256 nb2) public pure returns (uint256) {
+    function add(int256 nb1, int256 nb2) public pure returns (int256) {
         return nb1 + nb2;
     }
 
-    function sub(uint256 nb1, uint256 nb2) public pure returns (uint256) {
+    function sub(int256 nb1, int256 nb2) public pure returns (int256) {
         return nb1 - nb2;
     }
 
-    function mul(uint256 nb1, uint256 nb2) public pure returns (uint256) {
+    function mul(int256 nb1, int256 nb2) public pure returns (int256) {
         return nb1 * nb2;
     }
 
-    function div(uint256 nb1, uint256 nb2) public pure returns (uint256) {
+    function div(int256 nb1, int256 nb2) public pure returns (int256) {
         require(nb2 != 0, "Calculator: cannot divide by zero");
         return nb1 / nb2;
     }
 
-    function mod(uint256 nb1, uint256 nb2) public pure returns (uint256) {
+    function mod(int256 nb1, int256 nb2) public pure returns (int256) {
         return nb1 % nb2;
     }
 }
